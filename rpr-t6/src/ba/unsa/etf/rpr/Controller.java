eaafb760eb977b8c26685b1945807f98580fac78
tsijercic1 <tsijercic1@etf.unsa.ba>
2018-11-28 02:07:47 +0100

    Added ComboBox

--- rpr-t6/src/ba/unsa/etf/rpr/Controller.java
+++ rpr-t6/src/ba/unsa/etf/rpr/Controller.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public TextField name;
+    public TextField surname;
+    public TextField index;
+    public Button evaluateBtn;
+    public CheckBox cBox;
 }
